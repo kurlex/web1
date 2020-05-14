88cf83873e83cef4d06b37017a369e2934e191a5
kurlex <62076382+kurlex@users.noreply.github.com>
2020-05-14 23:39:38 +0100

    Update j.js

--- social-media-dashboard-with-theme-switcher-master/j.js
+++ social-media-dashboard-with-theme-switcher-master/j.js
@@ -1,37 +1,73 @@
 dd();
+function fgrey(){
+this.style.backgroundColor="#e1e3f0";
+}
+function fwhite(){
+this.style.backgroundColor="#f0f3fa";
+}
+function fgrey_insta(){
+  this.classList.remove("light");
+  this.classList.add("light-clicked");
+}
+function fwhite_insta(){
+  this.classList.remove("light-clicked");
+  this.classList.add("light");
+}
 function ll(){
   var y=document.getElementsByClassName("item");
-  for(i=0;i<4;i++){
-    y[i].addEventListener("mouseover",function(){
-    this.style.backgroundColor="#e1e3f0";
-  })
-    y[i].addEventListener("mouseout",function(){
-    this.style.backgroundColor="#f0f3fa";
-  })
+  for(i=0;i<4;i++)
+  if(i!=2){
+    y[i].addEventListener("mouseover",fgrey)
+    y[i].addEventListener("mouseout",fwhite)
+    y[i].removeEventListener("mouseover",ldark)
+    y[i].removeEventListener("mouseout",ddark)
   }
-  y[3].addEventListener("mouseover",function(){
-
-    this.classList.remove("light");
-    this.classList.add("light-clicked");
-  })
-    y[3].addEventListener("mouseout",function(){
-
-      this.classList.add("light");
-      this.classList.remove("light-clicked");
-    })
+  y[2].addEventListener("mouseover",fgrey_insta)
+  y[2].addEventListener("mouseout",fwhite_insta)
+  y[2].removeEventListener("mouseover",ldark_insta)
+  y[2].removeEventListener("mouseout",ddark_insta)
+y=document.getElementsByClassName("item1");
+for(i=0;i<8;i++){
+  y[i].addEventListener("mouseover",fgrey);
+  y[i].addEventListener("mouseout",fwhite);
+  y[i].removeEventListener("mouseover",ldark);
+  y[i].removeEventListener("mouseout",ddark);
+}
+}
+function ldark(){
+this.style.backgroundColor="#333a56";
+}
+function ddark(){
+this.style.backgroundColor="#252b43";
+}
+function ldark_insta(){
+  this.classList.remove("dark");
+  this.classList.add("dark-clicked");
+}
+function ddark_insta(){
+  this.classList.remove("dark-clicked");
+  this.classList.add("dark");
 }
 function dd(){
   var y=document.getElementsByClassName("item");
   for(i=0;i<4;i++)
-  if(i!=3){
-    y[i].addEventListener("mouseover",function(){
-    this.style.backgroundColor="#333a56";
-  })
-    y[i].addEventListener("mouseout",function(){
-    this.style.backgroundColor="#252b43";
-  })
+  if(i!=2){
+    y[i].removeEventListener("mouseover",fgrey)
+    y[i].removeEventListener("mouseout",fwhite)
+    y[i].addEventListener("mouseover",ldark);
+    y[i].addEventListener("mouseout",ddark);
   }
-
+  y[2].removeEventListener("mouseover",fgrey_insta)
+  y[2].removeEventListener("mouseout",fwhite_insta)
+  y[2].addEventListener("mouseover",ldark_insta);
+  y[2].addEventListener("mouseout",ddark_insta);
+  y=document.getElementsByClassName("item1");
+  for(i=0;i<8;i++){
+    y[i].addEventListener("mouseover",ldark);
+    y[i].addEventListener("mouseout",ddark);
+    y[i].removeEventListener("mouseover",fgrey);
+    y[i].removeEventListener("mouseout",fwhite);
+  }
 }
 document.getElementsByClassName("slider")[0].addEventListener("click",function(){
   var x=document.getElementById("d-l");
